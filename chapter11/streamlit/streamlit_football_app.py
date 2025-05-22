64aadc8a1e3b9bf13b9a053070430b564f23224e
Francisco Urra <urra.francisco@googlemail.com>
2025-05-22 09:29:05 +0000

    streamlit main file

---
+++ chapter11/streamlit/streamlit_football_app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import logging
+import pandas as pd
+
+if 'base_url' not in st.session_state:
+    st.session_state['base_url'] = 
+
+logging.basicConfig(
+    filename = 'football_app.log',
+    level = logging.INFO
+)
+
+st.set_page_config(page_tittle = "Football App",
+                   page_icon=":material/sports_football:")
+
+page1 = st.Page("page1.py", title = "Team Rosters", icon = ":material/trophy:")
+
+page2 = st.Page("page2.py", title = "Team Stats",
+                icon = ":material/star_border:")
+
+pg = st.navigation([page1, page2])
+pg.run()
