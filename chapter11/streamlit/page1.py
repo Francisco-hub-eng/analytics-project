f602492386d80a4f7255b74c199fba909a3580d6
Francisco Urra <urra.francisco@googlemail.com>
2025-05-22 09:56:32 +0000

    display data

--- chapter11/streamlit/page1.py
+++ chapter11/streamlit/page1.py
@@ -24,6 +24,33 @@ try:
         if 'unique_leagues' not in st.session_state:
             st.session_state['unique_leagues'] = unique_leagues
 
+        selected_league = st.sidebar._selectbox('Picj league ID', unique_leagues)
+
+        st.sidebar.divider()
+        st.sidebar.subheader(":blue[Data sources]")
+        st.sidebar.text("SportsWorldCentral")
+
+        flat_team_df = pd.json_normalize(
+            team_data, 'players', ['team_id', 'team_name', 'league_id']
+        )
+        column_order = ['league_id', 'team_id', 'team_name', 'position',
+                        'player_id', 'gsis_id', 'first_name', 'last_name']
+        flat_team_df_ordered = flat_team_df[column_order]
+
+        if 'flat_team_df_ordered' not in st.session_state:
+            st.session_state['flat_team_df_ordered'] = flat_team_df_ordered
+
+        display_df = flat_team_df_ordered.drop(columns=['team_id', 'player_id'])
+
+        display_df['league_id'] = display_df['league_id'].astype(str)
+        display_df = display_df[display_df['league_id']==selected_league]
+
+        st.dataframe(display_df, hide_index=True)
+
+    else:
+        logger.error(f"Error encountered: {team_api_response.status_code} {team_api_response.text}")
+        st.write("Error encountered while accessing data source.")
+
 except Exception as e:
-    st.subheader("An error occurred")
-    logging.error(f"An error occurred: {e}")
+    logger.error(f"Exception encountered: {str(e)}")
+    st.write(f"An unexpected error occurred.")
